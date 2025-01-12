e53f5cb191e34f101a4b44def416a2bea0952ec3
Joel <180830289+EhJoel@users.noreply.github.com>
2025-01-12 14:01:51 +1100

    homepageupdate

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,10 +9,17 @@ function Contact() {
       style= {{ backgroundImage: `url(${PuppyImg}`}}>
       </div>
       <div className='contactRight'>
-        <h1>Contact Us</h1>
+        <h1 className="contactH1">Contact Us</h1>
+
+        <ul className="contactUL">
+            <li>Joel Snell & Renae Nielsen</li>
+            <li><b>Phone</b> 0497 684 188</li>
+            <li><b>Email</b> renae.nielsen@bigpond.com</li>
+            <li><b>Location</b> Wesley Vale, Tasmania, 7307</li>
+        </ul>
 
         <form id="contactForm" method="POST">
-            
+
             <label htmlFor='name'>Full Name</label>
             <input name="name" placeholder="Enter full name" type="text" />
             
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import {Link} from"react-router-dom";
 import BannerImage from "../assets/rosanay.jpg";
+import BannerImage2 from "../assets/Blackpuppy.jpg"
 import '../styles/Home.css';
 
 function Home() {
   return (
-    <div className="home" 
-    style= {{ backgroundImage: `url(${BannerImage}`}}>
-        <div className="headerContainer">
-            <h1>Karenda Labradors</h1>
-            <p> Striving for success</p>
-            <Link to="contact" />
-            <button> Contact Us </button>
+    <><div className="home">
+      <div className="homeLeft"
+        style={{ backgroundImage: `url(${BannerImage}` }}>
+      </div>
+      <div className='homeRight'>
+        <h1>Karenda Labradors</h1>
+        <p className="homeText"> We are reputable Registered Breeders (Dogs Australia) of Labrador Retrievers and local members of Dogs Tasmania.
+          We strive to breed and show quality Labradors, focusing on breeding dogs with only exceptional temperaments, soundness and producing well-rounded family-friendly dogs.
+          We regularly exhibit our dogs in the show ring both in Tasmania and interstate.
+          We are based in Wesley Vale, Tasmania, on a small acreage property.</p>
+          <Link to="/Contact">
+          <button className="homeButton">Contact Us</button>
+          </Link>
+      </div> 
+
+
+
+    </div><div className="home">
+        <div className="homeLeft2">
+        <h1>Puppies</h1>
+          <p className="homeText"> We strive to breed and show quality Labradors,
+             focusing on breeding dogs with only exceptional temperaments,
+              soundness and producing well-rounded family-friendly dogs.
+                </p>
+                <Link to="/Contact">
+                <button className="homeButton">Puppy Enquiry </button>
+                </Link>
         </div>
-      
-    </div>
+        <div className='homeRight2' style={{ backgroundImage: `url(${BannerImage2}` }}>
+         
+        </div>
+
+      </div></>
+    
   );
 }
 
 export default Home
+
+
+
